517d8e474a1462b3285124d1c5ede45cb6fad900
deadog911@gmail.com <deadog911@gmail.com>
2023-01-15 13:36:09 +0300

    Добавьте файлы проекта.

---
+++ GraphParser/Parsing.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace GraphParser
+{
+    public class CVertex
+    {
+        public int id { get; set; }
+        public string info { get; set; }
+        public List<int> adj_v { get; set; }
+        public CVertex(int id, string info="", List<int> adj_v=null)
+        {
+            this.id = id;
+            this.info = info;
+            this.adj_v = adj_v;
+        }        
+    }
+    public class CServer
+    {
+        public int id { get; set; }
+        public string address { get; set; }
+    }
+
+    public class CEdge
+    {
+        public int id { get; set; }
+        public int weight { get; set; }
+    }
+    public class CAdjVertex
+    {
+        public int id { get; set; }
+        public List<CEdge> edges { get; set; }
+    }
+     public class Config
+    {
+        public List<CVertex> vertexes { get; set; }
+        public List<CServer> servers { get; set; }
+        public List<CAdjVertex> adj_list { get; set; }
+        public Config()
+        {
+            vertexes = new List<CVertex>();
+            servers = new List<CServer>();
+            adj_list = new List<CAdjVertex>();
+        }
+    }
+    class RedactorVertex
+    {
+        public string name { get; set; }
+        public string color { get; set; }
+    }
+    class RedactorEdge
+    {
+        public int vertex1 { get; set; }
+        public int vertex2 { get; set; }
+        public string weight { get; set; }
+    }
+    class RedactorConfig
+    {
+        public List<RedactorVertex> vertices { get; set; }
+        public List<RedactorEdge> edges { get; set; }
+    }
+    class Parsing
+    {
+        public static RedactorConfig parse(string filename)
+        {
+            RedactorConfig config;
+            using (StreamReader file = File.OpenText(filename))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                config = (RedactorConfig) serializer.Deserialize(file, typeof(RedactorConfig));
+            }
+            return config;
+        }
+    }
+}
---
+++ GraphParser/Program.cs
@@ -0,0 +1,56 @@
+﻿using GraphParser;
+using System.Net.Http.Headers;
+
+RedactorConfig rconfig = Parsing.parse("graph.graph");
+Config[] configs = new Config[] {
+    new Config(),
+    new Config(),
+    new Config()
+};
+
+List<CVertex>[] serversVertices = new List<CVertex>[] {
+    new List<CVertex>(),
+    new List<CVertex>(),
+    new List<CVertex>()
+};
+
+List<CEdge>[] serversEdges = new List<CEdge>[] {
+    new List<CEdge>(),
+    new List<CEdge>(),
+    new List<CEdge>()
+};
+
+Dictionary<int, List<RedactorEdge>> adj_lists = new Dictionary<int, List<RedactorEdge>>();
+
+/*
+ * 1. Парсим .graph
+ * 2. Составляем список из вершин
+ * 3. Составляем список из рёбер (А лучше список смежности по первой вершине ребра)
+ * 4. Проходимся по рёбрам:
+ * 4.1. Для первого ребра добавляем обе вершины в список вершин для первого подграфа
+ * 4.2. Далее находим все инцидентные ребра для вершин, если они не гранчиащие
+ *      и добавляем вторую вершину ребра в список для первого графа
+ * 4.3. Если не граничащие вершины закончились, переходим к граничащим.
+ * 4.4. Проделываем то же самое, только уже для второго подграфа.
+ */
+
+// Создаем списки смежности
+foreach (RedactorEdge rv in rconfig.edges)
+{
+    if (adj_lists.ContainsKey(rv.vertex1))
+    {
+        adj_lists[rv.vertex1].Add(rv);
+    }
+    else
+    {
+        adj_lists.Add(rv.vertex1, new List<RedactorEdge> { rv });
+    }
+}
+
+int server_i = 0;
+foreach (RedactorEdge rv in rconfig.edges)
+{
+    serversVertices[server_i].Add(new CVertex(rv.vertex1));
+    serversVertices[server_i].Add(new CVertex(rv.vertex2));
+    
+}
